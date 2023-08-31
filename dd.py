cb7a05e10a0def3151ae69d08511777c7c9b506f
ndh12345 <harrypotter041112@naver.com>
2023-08-31 20:59:15 +0900

    단축키
    alt+1누르면 hello

--- dd.py
+++ dd.py
@@ -1,1 +1,20 @@
-a= 3
+from pynput.keyboard import Listener, Key, KeyCode, Controller
+store= set()
+def he():
+    print('hello')
+HOT_KEYS= {
+    he: set([Key.alt_l, KeyCode(char= '1')])
+}
+def ha(key):
+    store.add(key)
+def a(key):
+    for action, trigger in HOT_KEYS.items():
+        CHECK= all([True if triggerKey in store else False for triggerKey in trigger])
+        if CHECK:
+            action()
+    if key in store:
+        store.remove(key)
+    if key==Key.esc:
+        return False
+with Listener(on_press= ha, on_release= a) as ls:
+    ls.join()
